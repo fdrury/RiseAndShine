f68e1b421ae1026823a26b86715142094922ac44
fdrury <fdrury@ualberta.ca>
2016-11-13 04:03:11 -0700

    Ahhhhh make the noise stop!
    I tried unplugging the power and instantly regretted installing the battery backpack

---
+++ TestFiles/digitalBuzzerTest.ino
@@ -0,0 +1,21 @@
+int delayTime = 100;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  pinMode(0, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delayTime += 1;
+  if(delayTime > 300)
+    delayTime = 100;
+  
+  digitalWrite(2, HIGH);
+  digitalWrite(0, LOW);
+  delayMicroseconds(delayTime);
+  digitalWrite(2, LOW);
+  digitalWrite(0, HIGH);
+  delayMicroseconds(delayTime);
+}
